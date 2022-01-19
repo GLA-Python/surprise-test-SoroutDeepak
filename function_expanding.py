366e949d93f98f241546a9ba93c3bf218facb6fe
Deepak Sorout <91543427+SoroutDeepak@users.noreply.github.com>
2022-01-19 07:44:29 -0800

    Update function_expanding.py

--- function_expanding.py
+++ function_expanding.py
@@ -1,17 +1,16 @@
 def expanding(lst):
-    lst2 = []
-    for i in range(1, len(lst)):
-        x = lst[i] - lst[i-1]
+    lst2=[]
+    for i in range(1,len(lst)):
+        x=lst[i]-lst[i-1]
         lst2.append(abs(x))
-    y = True
-    for i in range(1, len(lst2)):
+    y=True
+    for i in range(1,len(lst2)):
         if lst2[i]<=lst2[i-1]:
-            y = False
+            y=False
             break
-
-
+    
     return y
 
 
-lst = list(map(int,input().split))
+lst=list(map(int,input().split()))
 print(expanding(lst))
